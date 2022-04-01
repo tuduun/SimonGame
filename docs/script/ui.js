3d0c51fead064a00074192d283d17e6c1a3cdbf5
tuduun <gantulga01@allegheny.edu>
2022-04-01 16:17:21 -0400

    Fix the index.html

--- docs/script/ui.js
+++ docs/script/ui.js
@@ -1,1 +1,6 @@
-// TODO: Leave this alone until prompted!
+let green = document.getElementById("top-right");
+green.addEventListener("click", () => {
+    document.getElementById("interactButton").innerText = "Points: 0";
+    green.style.backgroundColor = "#FF3898";
+    console.log("MADE IT HERE");
+});

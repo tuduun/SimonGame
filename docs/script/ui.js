7a03dbb84ba58572d1dd365204e7821977479cee
tuduun <gantulga01@allegheny.edu>
2022-04-09 21:56:27 -0400

    Add class work

--- docs/script/ui.js
+++ docs/script/ui.js
@@ -1,6 +1,97 @@
-let green = document.getElementById("top-right");
-green.addEventListener("click", () => {
-    document.getElementById("interactButton").innerText = "Points: 0";
-    green.style.backgroundColor = "#FF3898";
-    console.log("MADE IT HERE");
+var gamebuttons = document.querySelectorAll(".buttonClass");
+var userChoices = [];
+var gamePattern = [];
+var level = 0;
+var points = 0;
+// set delay for user response
+
+const delay = 4000;
+
+// functions
+
+// random button ID generator.
+const chooseRandomBtn = () => {
+    let choice = Math.floor(Math.random() * elements.length);
+    // debug codes
+    // console.log(choice);
+    // console.log(elements[choice]);
+    return elements[choice]
+};
+
+const validatePattern = () => {
+    if(userChoices.length !== gamePattern.length){
+        return false
+    }
+    for (var i = 0; i < userChoices.length; i++){
+        if(userChoices[i] !== gamePattern[i]) return false
+    }
+    return true
+}
+
+function playTurn(){
+    let level = 0
+    userChoices = [];
+    gamePattern.push(chooseRandomBtn());
+    const turn = setInterval(() =>{
+        let id = gamePattern[level];
+        activateButton(id);
+        level++;
+        if(level >= gamePattern.length){
+            clearInterval(turn);
+        }
+    },1000);
+    setTimeout(() =>{
+        if(validatePattern()){
+            playTurn();
+            points++;
+            document.getElementById("interactButton").innerText = `Points: ${points}`;
+        }else{
+            console.log("You lose")
+        }
+    }, gamePattern.length * delay);
+}
+
+function activateButton(id){
+    const elem = document.getElementById(id);
+    const origBg = elem.style.backgroundColor;
+    setTimeout(()=>{
+        elem.style.backgroundColor = "#FFF";
+    }, 500);
+    setTimeout(()=>{
+        elem.style.backgroundColor = origBg;
+    }, 750);
+}
+
+
+
+// Put all the IDs in an array
+const elements = Array.from(gamebuttons).map(button => {
+    return button.id;
 });
+
+//for every game button
+gamebuttons.forEach(elem => {
+    elem.addEventListener("click", (evt) => {
+        let source = evt.target.id;
+        userChoices.push(source);
+        activateButton(source)
+        const data = elem.getAttribute("data-button");
+        const sound = document.querySelector(`[data-sound = '${data}']`);
+        sound.play();
+    });
+});
+
+var startButton = document.getElementById("interactButton")
+startButton.addEventListener("click", () => {
+    document.getElementById("interactButton").innerText = `Points: ${points}`;
+    startButton.style.backgroundColor = "#FF3898";
+    userChoices = [];
+    gamePattern = [];
+    playTurn();
+});
+
+
+// function calls
+
+gamePattern.push(chooseRandomBtn());
+console.log(gamePattern)
